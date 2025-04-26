5a4a40bfd95ae088896726b00b57996f7f24bb1e
XiaoWang1212 <danny20041212@gmail.com>
2025-04-26 17:54:38 +0800

    backend test2

--- src/amplify-config.js
+++ src/amplify-config.js
@@ -1,7 +1,23 @@
-// src/amplify-config.js
 import { Amplify } from 'aws-amplify';
-import amplifyconfig from './amplifyconfiguration.json';
 
-Amplify.configure(amplifyconfig);
+try {
+  const amplifyconfig = {
+    "aws_project_region": "us-west-2",
+    "Auth": {
+      "region": "us-west-2",
+      "userPoolId": "us-west-2_adUgt5d9m",
+      "userPoolWebClientId": "35e3m2vlp4ijca2agas9ekula0",
+      "identityPoolId": "us-west-2:cd7301c7-7170-4460-a40b-ec51a74e75c4",
+      "mandatorySignIn": false,
+      "authenticationFlowType": "USER_SRP_AUTH"
+    }
+  };
+  
+  // 配置 Amplify
+  Amplify.configure(amplifyconfig);
+  console.log('Amplify 配置成功載入');
+} catch (error) {
+  console.error('Amplify 配置載入失敗:', error);
+}
 
 export default Amplify;
